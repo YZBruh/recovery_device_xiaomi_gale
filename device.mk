132975b6f2ae445c251456d290bcb13468e6c2e6
YZBruh <yagizzengin73@gmail.com>
2024-02-18 20:03:46 +0300

    Apply updates based on OTA and A/B

--- device.mk
+++ device.mk
@@ -22,12 +22,23 @@ AB_OTA_PARTITIONS += \
     sspm \
     tee
 
+# Update Engine & Update Verifier 
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+
+# OTA
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh
 
 # Virtual A/B
 ENABLE_VIRTUAL_AB := true
@@ -38,10 +49,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service \
     android.hardware.boot@1.2-mtkimpl \
     android.hardware.boot@1.2-mtkimpl.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl \
-    update_engine_client
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6768 \
